a56e4db821ff5c8e03b6cc47ba277e98e5e2a0c3
Arseni Barsegyan <arseni.barsegyan@gmail.com>
2018-06-05 17:21:57 +0300

    Added Sorting project. Added BubbleSort, SelectionSort

---
+++ Algorithms/Sorting/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Sorting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new[] { 6, 7, 34, 12, 16, 69, 10 };
+            SelectionSort(array);
+
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        // Bubble sorting algorithm. Worst-case and average complexity of O(n2). 
+        // Ineffecient for large amount of data.
+        private static void BubbleSort(int[] array)
+        {
+            bool flag = false;
+
+            do
+            {
+                flag = false;
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+            while (flag);
+        }
+
+        // Selection sorting algorithm. Complexity is O(n2) for best, average, and worst case scenarios.
+        // Ineffecient for large amount of data.
+        private static void SelectionSort(int[] array)
+        {
+            int minIndex;
+            int temp;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                minIndex = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+                }
+            }
+        }
+
+        private static void InsertionSort(int[] array)
+        {
+
+        }
+    }
+}
